9b5cfdabf9424b40649d8f59f593acc89a3a9f22
Furpu <feripuru@proton.me>
2024-09-06 07:29:01 -0300

    Return unused imports ordered by source location

--- src/zigimports.zig
+++ src/zigimports.zig
@@ -8,6 +8,12 @@ pub const ImportSpan = struct {
     start_column: usize,
     end_line: usize,
     end_column: usize,
+
+    // Compare ImportSpans by their source location.
+    fn lessThanByLocation(_: void, a: ImportSpan, b: ImportSpan) bool {
+        if (a.start_line == b.start_line) return a.start_column < b.start_column;
+        return a.start_line < b.start_line;
+    }
 };
 
 pub const BlockSpan = struct {
@@ -179,6 +185,9 @@ pub fn find_unused_imports(al: std.mem.Allocator, source: [:0]u8) !std.ArrayList
             try unused_imports.append(span);
         }
     }
+
+    // Order results by their source location.
+    std.sort.heap(ImportSpan, unused_imports.items, {}, ImportSpan.lessThanByLocation);
 
     return unused_imports;
 }
