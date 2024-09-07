6ac3f0268534698ae8113f8201d857e3a2b1d4b6
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-09-07 06:09:18 +0530

    Add debug logging

--- src/zigimports.zig
+++ src/zigimports.zig
@@ -64,7 +64,7 @@ fn is_inside_block(blocks: []BlockSpan, source_pos: usize) bool {
     return false;
 }
 
-pub fn find_unused_imports(al: std.mem.Allocator, source: [:0]u8) !std.ArrayList(ImportSpan) {
+pub fn find_unused_imports(al: std.mem.Allocator, source: [:0]u8, debug: bool) !std.ArrayList(ImportSpan) {
     var tree = try std.zig.Ast.parse(al, source, .zig);
     defer tree.deinit(al);
 
@@ -98,6 +98,18 @@ pub fn find_unused_imports(al: std.mem.Allocator, source: [:0]u8) !std.ArrayList
                     .start_index = tree.tokenToSpan(lbrace).start,
                     .end_index = tree.tokenToSpan(rbrace).end,
                 });
+                if (debug) {
+                    const lbrace_location = tree.tokenLocation(0, lbrace);
+                    const rbrace_location = tree.tokenLocation(0, rbrace);
+                    std.debug.print("Block statement from {}:{} ({}) to {}:{} ({})\n", .{
+                        lbrace_location.line + 1,
+                        lbrace_location.column,
+                        tree.tokenToSpan(lbrace).start,
+                        rbrace_location.line + 1,
+                        rbrace_location.column + tree.tokenSlice(rbrace).len,
+                        tree.tokenToSpan(rbrace).end + tree.tokenSlice(rbrace).len,
+                    });
+                }
             },
             else => {},
         }
@@ -109,16 +121,30 @@ pub fn find_unused_imports(al: std.mem.Allocator, source: [:0]u8) !std.ArrayList
         const import_stmt = tree.simpleVarDecl(@intCast(index));
         // Skip non-global declarations
         const first_token = tree.tokens.get(import_stmt.firstToken());
-        if (is_inside_block(block_spans.items, first_token.start))
+        if (is_inside_block(block_spans.items, first_token.start)) {
+            if (debug) {
+                std.debug.print("Skipping global on line {}; it's inside a block\n", .{
+                    tree.tokenLocation(0, import_stmt.firstToken()).line,
+                });
+            }
             continue;
+        }
 
         // Don't try to delete `pub`, `extern` and `export` statements
-        if (import_stmt.visib_token != null or import_stmt.extern_export_token != null)
+        if (import_stmt.visib_token != null or import_stmt.extern_export_token != null) {
+            if (debug) {
+                std.debug.print("Skipping import on line {} as it's pub/extern/export\n", .{
+                    tree.tokenLocation(0, import_stmt.firstToken()).line,
+                });
+            }
             continue;
+        }
 
         const import_name_idx = import_stmt.ast.mut_token + 1;
         const import_name = tree.tokenSlice(import_name_idx);
         try import_index.put(import_name, @intCast(index));
+        if (debug and import_used.get(import_name) == null)
+            std.debug.print("Found new global: {s}\n", .{import_name});
         try import_used.put(import_name, false);
     }
 
@@ -129,6 +155,8 @@ pub fn find_unused_imports(al: std.mem.Allocator, source: [:0]u8) !std.ArrayList
         const identifier = tree.tokenSlice(identifier_idx);
         if (import_used.getKey(identifier) != null) {
             // Mark import as used
+            if (debug and import_used.get(identifier) == false)
+                std.debug.print("Global {s} is being used\n", .{identifier});
             try import_used.put(identifier, true);
         }
     }
@@ -139,6 +167,7 @@ pub fn find_unused_imports(al: std.mem.Allocator, source: [:0]u8) !std.ArrayList
         const import_variable = entry.key_ptr.*;
         const is_used = entry.value_ptr.*;
         if (!is_used) {
+            if (debug) std.debug.print("Found unused identifier: {s}\n", .{import_variable});
             const node_index = import_index.get(import_variable).?;
 
             const first_token = tree.firstToken(node_index);
@@ -195,17 +224,40 @@ fn compare_start(_: void, lhs: ImportSpan, rhs: ImportSpan) bool {
     return false;
 }
 
-pub fn remove_imports(al: std.mem.Allocator, source: [:0]u8, imports: []ImportSpan) !std.ArrayList([]u8) {
+pub fn remove_imports(al: std.mem.Allocator, source: [:0]u8, imports: []ImportSpan, debug: bool) !std.ArrayList([]u8) {
     std.debug.assert(imports.len > 0);
     std.mem.sort(ImportSpan, imports, {}, compare_start);
 
     var new_spans = std.ArrayList([]u8).init(al);
     var previous_import = imports[0];
+    if (debug) {
+        std.debug.print("Unused import statement from {}:{} ({}) to {}:{} ({})\n", .{
+            previous_import.start_line,
+            previous_import.start_column,
+            previous_import.start_index,
+            previous_import.end_line,
+            previous_import.end_column,
+            previous_import.end_index,
+        });
+        std.debug.print("Keeping source from 0 to {}\n", .{previous_import.start_index});
+    }
     try new_spans.append(source[0..previous_import.start_index]);
     for (imports[1..]) |import| {
         try new_spans.append(source[previous_import.end_index..import.start_index]);
+        if (debug) {
+            std.debug.print("Unused import statement from {}:{} ({}) to {}:{} ({})\n", .{
+                import.start_line,
+                import.start_column,
+                import.start_index,
+                import.end_line,
+                import.end_column,
+                import.end_index,
+            });
+            std.debug.print("Keeping source from {} to {}\n", .{ previous_import.end_index, import.start_index });
+        }
         previous_import = import;
     }
     try new_spans.append(source[previous_import.end_index..]);
+    if (debug) std.debug.print("Keeping source from {} to the end.\n", .{previous_import.end_index});
     return new_spans;
 }
